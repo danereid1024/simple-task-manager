5908d78a9d57eac47eb7216781c2706ee56082ab
Dane Reid <forestlevelgames@gmail.com>
2024-06-07 23:04:50 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'simple-task-manager',
+  webDir: 'www'
+};
+
+export default config;
